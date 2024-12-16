77dda2d7a1b24f196b9b56849e95572d1b757081
Tawfiq Aliu <tawfiqaliu@gmail.com>
2024-12-16 15:12:28 -0500

    add code using imgur

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+NOTION_PAGE_ID = os.environ["NOTION_PAGE_ID"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+IMGUR_CLIENT_ID = os.environ["IMGUR_CLIENT_ID"]
+PDF_PATH = os.environ["PDF_PATH"]
